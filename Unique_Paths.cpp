9f1dd13727544e0ef965747a9e3f7115444e37da
Siva Krishna <119127579+sivakrishna1023@users.noreply.github.com>
2023-06-19 16:04:07 +0530

    Create Unique_Paths.cpp

---
+++ Unique_Paths.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+int uniquePaths(int m, int n) {
+	vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+	dp[1][1]=1;
+	for(int i=1;i<m+1;i++){
+		for(int j=1;j<n+1;j++){
+			if(i==1 && j==1) continue;
+			dp[i][j]=dp[i-1][j]+dp[i][j-1];
+		}
+	}
+	return dp[m][n];
+	// Write your code here.
+}
