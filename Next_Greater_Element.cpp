4ac8873ac95e9265ddf54fd88e1abaa03c9d4d84
Siva Krishna <119127579+sivakrishna1023@users.noreply.github.com>
2023-06-27 16:38:26 +0530

    Create Next_Greater_Element.cpp

---
+++ Next_Greater_Element.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> nextGreater(vector<int> &arr, int n) {
+    stack<int> vals;
+    vector<int> ans(n, 0);
+    if (n == 0) return ans;
+    for (int i = n - 1; i >= 0; i--) {
+        if (vals.empty()) {
+            ans[i] = -1;
+            vals.push(arr[i]);
+        } else if (vals.top() <= arr[i]) {
+            while (!vals.empty() && vals.top() <= arr[i]) {
+                vals.pop();
+            }
+            if (vals.empty())
+                ans[i] = -1;
+            else
+                ans[i] = vals.top();
+            vals.push(arr[i]);
+        } else {
+            ans[i] = vals.top();
+            vals.push(arr[i]);
+        }
+    }
+    return ans;
+}
